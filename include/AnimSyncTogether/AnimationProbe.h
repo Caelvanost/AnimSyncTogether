5f54e43a341ffb06858bfb00ca53f58f92730685
Caelvanost <caelvanost@gmail.com>
2026-08-18 17:45:15 +0200

    Remove proxy discovery from animation probe

--- include/AnimSyncTogether/AnimationProbe.h
+++ include/AnimSyncTogether/AnimationProbe.h
@@ -6,23 +6,16 @@
 
 namespace AnimSyncTogether
 {
-    class AnimationProbe final :
-        public RE::BSTEventSink<RE::BSAnimationGraphEvent>,
-        public RE::BSTEventSink<RE::TESObjectLoadedEvent>
+    class AnimationProbe final : public RE::BSTEventSink<RE::BSAnimationGraphEvent>
     {
     public:
         static AnimationProbe* GetSingleton();
 
         void Install();
-        void ScanForProxyActors();
 
         RE::BSEventNotifyControl ProcessEvent(
             const RE::BSAnimationGraphEvent* event,
             RE::BSTEventSource<RE::BSAnimationGraphEvent>* source) override;
-
-        RE::BSEventNotifyControl ProcessEvent(
-            const RE::TESObjectLoadedEvent* event,
-            RE::BSTEventSource<RE::TESObjectLoadedEvent>* source) override;
 
     private:
         AnimationProbe() = default;
@@ -30,6 +23,5 @@ namespace AnimSyncTogether
         bool AttachActor(RE::Actor* actor, const char* reason);
 
         std::unordered_set<RE::FormID> attachedActors_;
-        bool objectLoadWatcherInstalled_{ false };
     };
 }
