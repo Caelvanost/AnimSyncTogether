0a5dd3bcd9fcc13cad79bed8adcdc81114dd379d
Caelvanost <caelvanost@gmail.com>
2026-08-20 15:40:01 +0200

    Bump AnimSyncTogether to v0.9.0

--- include/AnimSyncTogether/Version.h
+++ include/AnimSyncTogether/Version.h
@@ -6,7 +6,7 @@
 namespace AnimSyncTogether::Version
 {
     inline constexpr std::uint32_t MAJOR = 0;
-    inline constexpr std::uint32_t MINOR = 8;
+    inline constexpr std::uint32_t MINOR = 9;
     inline constexpr std::uint32_t PATCH = 0;
-    inline constexpr std::string_view STRING = "0.8.0";
+    inline constexpr std::string_view STRING = "0.9.0";
 }
